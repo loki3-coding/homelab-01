7e3e761600d64f2c5791d1c2fc3a7f4e63a75213
SWE <18114521+loki3-coding@users.noreply.github.com>
2026-01-28 01:42:58 +0700

    update default init db immich script

--- immich/init-db.sql
+++ immich/init-db.sql
@@ -2,7 +2,7 @@
 -- Run this script as the PostgreSQL admin user to set up Immich database
 
 -- Create Immich user (change password as needed)
-CREATE USER immich WITH PASSWORD 'your_secure_password';
+CREATE USER immich WITH PASSWORD 'changeit';
 
 -- Create Immich database
 CREATE DATABASE immich OWNER immich;
